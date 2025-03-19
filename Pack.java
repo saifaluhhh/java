3742f305e9b5d0df15fba5ff9e130340e65be0af
saifaluhhh <webd26194@gmail.com>
2025-03-19 14:21:51 +0530

    Add files via upload

---
+++ Pack.java
@@ -0,0 +1,18 @@
+import mypackage.Demo;
+public class Pack {
+
+    String name = "abbasali";
+
+    public static void main(String[] args) {
+
+    Pack p1 = new Pack();
+        System.out.println(p1.name);
+        Demo d1 = new Demo();
+        d1.sample();
+
+
+    }
+
+
+
+}
